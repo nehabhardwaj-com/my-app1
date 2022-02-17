99a83f946f098db797e8be8934471f0ef94626e0
nehabhardwaj-com <nehabhardwaj690@gmail.com>
2022-02-17 12:34:39 +0000

    vdo 9

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,21 +3,54 @@ import React, { useState } from "react";
 export default function TextForm(props) {
 
   const handleUpClick = ()=>{
-    console.log('Button is clicked', text);
     let newText = text.toUpperCase();
     setText(newText)
     }
-    const handlelowerClick = ()=>{
-      console.log('Button is clicked', text);
+    const handlelowerClick = ()=>{      
       let newText = text.toLowerCase();
       setText(newText)
       }
+    const handleClearClick = ()=>{       
+        setwordCount(0);
+        setcharacter(0);
+        setCount(0);
+        let newText = " ";
+        setText(newText);
+        }
+        const handleFirstClick = ()=>{       
+         
+          let arr = text.split(". ");
+          for (var i = 0; i < arr.length; i++) {
+            arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);        
+        }
+        const str2 = arr.join(". ");
+          setText(str2);
+          }
+    const handleCopyClick = ()=>{
+          let newText = text;
+          setcopiedText(newText);
+          setText(newText);
+          }
+    const handlecountClick = ()=>{
+            let newText =text.split("d").length;
+            setCount(newText);
+            // setText(newText);
+            }
+    const handlePasteClick = ()=>{
+              setText(copiedText);
+            }
   
     const handleOnChange = (event)=>{
       console.log("onchange");
-  setText(event.target.value)
+      setwordCount(text.split(" ").length)
+      setcharacter(text.length)
+      setText(event.target.value)
       }
-  const [text, setText] = useState(""); 
+  const [text, setText] = useState("");   
+  const [wordCount, setwordCount] = useState(0);
+  const [character, setcharacter] = useState(0);   
+  const [copiedText, setcopiedText] = useState("");
+  const [count, setCount] = useState(0);   
   return (
     <><div>
        <div className="mb-3">
@@ -26,17 +59,22 @@ export default function TextForm(props) {
        </div>
       <button className="btn btn-primary" onClick = {handleUpClick} >Convert to Uppercase</button>
       <button className="btn btn-primary mx-3" onClick = {handlelowerClick} >Convert to Lowercase</button>
+      <button className="btn btn-primary mx-3" onClick = {handleFirstClick} >First Letter Uppercase</button>
+      <button className="btn btn-primary mx-3" onClick = {handleClearClick} >Clear Text</button>
+      <button className="btn btn-primary mx-3" onClick = {handleCopyClick} >Copy</button>
+      <button className="btn btn-primary mx-3" onClick = {handlePasteClick} >Paste</button>
+      <button className="btn btn-primary mx-3" onClick = {handlecountClick} >Count</button>
     </div>
     <div className="container my-3">
       <h2>Your text summary</h2>
       <p>
-        {text.split(" ").length} words and {text.length} characters
+        {wordCount} words and {character} characters
       </p>
-      <p>{0.008 * text.split(" ").length} Minutes to Read</p>
+      <p>{0.008 * wordCount} Minutes to Read</p>
     </div>
     <div className="container my-3">
       <h2>Preview</h2>
-      <p>{text}</p>
+      <p>{text}{count}</p>
     </div>
     </>
 
